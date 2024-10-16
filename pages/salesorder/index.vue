388fb13edd14d6d3ff69b6a01e9e65144e6aa9fa
andisulistyonugroho <andisulistyonugroho@gmail.com>
2024-10-17 04:57:21 +0700

    style: change new to add

--- pages/salesorder/index.vue
+++ pages/salesorder/index.vue
@@ -4,7 +4,7 @@ const tab = ref(1)
 <template>
   <v-tabs v-model="tab" align-tabs="start" color="primary" class="mt-0 mb-3">
     <v-tab :value="1" to="/salesorder">List</v-tab>
-    <v-tab :value="2" to="/salesorder/add">New</v-tab>
+    <v-tab :value="2" to="/salesorder/add">Add</v-tab>
   </v-tabs>
   <v-container fluid>
 
